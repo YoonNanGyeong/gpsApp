a5198ee028de8e3786e672d6aab17ef41d5439c0
YoonNanGyeong <nangyung97@gmail.com>
2023-10-10 14:09:46 +0900

    first commit - layout

---
+++ app/src/main/java/com/ntek/testgpsapp/Login.java
@@ -0,0 +1,20 @@
+package com.ntek.testgpsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+public class Login extends AppCompatActivity {
+    TextView sign;
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+
+
+    }
+
+
+}
