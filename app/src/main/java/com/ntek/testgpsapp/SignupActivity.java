54d7f35d95bcc1c3cba6a28e2ddf35839ed42619
YoonNanGyeong <nangyung97@gmail.com>
2023-10-12 03:00:40 +0900

    SignupActivity에 db 인스턴스 생성 추가

--- app/src/main/java/com/ntek/testgpsapp/SignupActivity.java
+++ app/src/main/java/com/ntek/testgpsapp/SignupActivity.java
@@ -3,10 +3,13 @@ package com.ntek.testgpsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.room.Room;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+
+import com.ntek.testgpsapp.DAO.UserDAO;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -15,8 +18,16 @@ public class SignupActivity extends AppCompatActivity {
         super.onPostCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        // appbar 설정
         getSupportActionBar().setTitle("회원가입");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // 데이터베이스 인스턴스 생성
+        AppDatabase db = Room.databaseBuilder(getApplicationContext(),
+                AppDatabase.class, "AppDatabase").build();
+
+        UserDAO userDAO = db.userDao(); //DAO 인스턴스
+        userDAO.insertAll();    //회원정보 저장
 
     }
 
