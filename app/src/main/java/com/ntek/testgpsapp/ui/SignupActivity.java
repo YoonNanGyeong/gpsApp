bd9623d215d8d2f29c4acf218fb3edafc4e33e44
YoonNanGyeong <nangyung97@gmail.com>
2023-10-19 16:33:07 +0900

    구글 맵 라이브러리 종속성 추가

--- app/src/main/java/com/ntek/testgpsapp/ui/SignupActivity.java
+++ app/src/main/java/com/ntek/testgpsapp/ui/SignupActivity.java
@@ -19,7 +19,6 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
